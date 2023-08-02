2f4b2e6a226d31c844d812705984879492d4052c
MrUnderSan <MrUnderSan@gmail.com>
2023-08-02 17:37:47 +0300

    add adaptive to SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
+  
+  ${font({family: "'Josefin Sans',sans-serif", weight: 600, fMin: 30, fMax: 36})}
   text-align: center;
   margin-bottom: 90px;
-  font-family: Josefin Sans,sans-serif;
-  font-size: 36px;
-  font-weight: 600;
   letter-spacing: 5px;
   
   position: relative;
@@ -23,5 +23,13 @@ export const SectionTitle = styled.h2`
     right: 50%;
     transform: translateX(50%);
 
+    @media ${theme.media.mobile} {
+      bottom: -24px;
+    }
+
+  }
+  
+  @media ${theme.media.mobile} {
+    margin-bottom: 75px;
   }
 `
