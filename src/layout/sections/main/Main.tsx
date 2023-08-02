c044d4a1d38c6b717f53556f1aebe730b6cac1c8
MrUnderSan <MrUnderSan@gmail.com>
2023-08-02 16:17:58 +0300

    add adaptive to Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,15 +4,16 @@ import photo from '../../../assets/images/photo.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
-                        <Name>I am <span>Aliaksandr Yurhilevich</span></Name>
+                        <Name>I am <NameSpan><span>Aliaksandr</span> <span>Yurhilevich</span></NameSpan></Name>
                         <MainTitle>A Web Developer.</MainTitle>
                     </div>
                     <PhotoWrapper>
@@ -29,10 +30,27 @@ const StyledMain = styled.section`
   min-height: 100vh;
   background-color: #ffc8c8;
   display: flex;
+  padding-top: 100px;
+
+  @media screen and (max-width: 930px) {
+    ${FlexWrapper} {
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  @media screen and (max-width: 360px) {
+    ${FlexWrapper} {
+      align-items: flex-start;
+    }
+  }
+  
 `
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;
   
   &::before {
     content: "";
@@ -44,6 +62,14 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+
+
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+    }
   }
 `
 
@@ -51,36 +77,61 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+
+  @media screen and (max-width: 1260px) {
+    margin-right: 40px;
+  }
+  
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+    margin-right: 35px;
+  }
 `
 
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
+  ${font({weight: 400, fMax: 27, fMin: 20})}
+
 `
 
 const Name = styled.p`
-  font-family: Josefin Sans,sans-serif;
-  font-size: 50px;
-  font-weight: 700;
+
+  ${font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 50, fMin: 36})}
+  
   letter-spacing: 2.5px;
   margin: 10px 0;
   
-  span {
+
+  
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
+`
+
+const NameSpan = styled.span `
     position: relative;
     z-index: 0;
-    
+
+
     &::before {
-      content: "";
-      display: inline-block;
-      height: 20px;
-      width: 100%;
-      background-color: ${theme.colors.accent};
-      
-      position: absolute;
-      bottom: 0;
-      z-index: -1;
+        content: "";
+        display: inline-block;
+        height: 20px;
+        width: 100%;
+        background-color: ${theme.colors.accent};
+
+        position: absolute;
+        bottom: 0;
+        z-index: -1;
     }
-  }
+
+    @media screen and (max-width: 1140px) {
+        display: flex;
+    }
+    
+    @media screen and (max-width: 490px) {
+        flex-direction: column;
+    }
 `
 
 const SmallText = styled.span`
--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -22,6 +22,8 @@ export const GlobalStyle = createGlobalStyle `
     @media (prefers-reduced-motion: reduce) {
        animation: unset;
     }
+    
+    min-width: 360px;
   }
   
   a {
@@ -42,6 +44,10 @@ export const GlobalStyle = createGlobalStyle `
   
   section {
     padding: 100px 0;
+    
+    @media ${theme.media.mobile} {
+      padding: 80px 0;
+    }
   }
   
   section:nth-of-type(odd) {
