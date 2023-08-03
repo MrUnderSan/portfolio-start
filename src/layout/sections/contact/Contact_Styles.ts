92f00e980fa5c1c27ae22e6a87df71d39af9b159
MrUnderSan <MrUnderSan@gmail.com>
2023-08-03 14:41:47 +0300

    refactor Contact

---
+++ src/layout/sections/contact/Contact_Styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+const Contact = styled.section `
+`
+
+const Form = styled.form`
+  max-width: 540px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  margin: 0 auto;
+  
+  textarea {
+    resize: none;
+    height: 155px;
+  }
+`
+
+const Field = styled.input`
+  width: 100%;
+  background-color: ${theme.colors.secondaryBg};
+  border: 1px solid ${theme.colors.borderColor};
+  padding: 7px 15px;
+
+  color: ${theme.colors.font};
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.6px;
+  font-family: Poppins,sans-serif;
+  
+  &::placeholder {
+    color: ${theme.colors.placeHolderColor};
+    text-transform: capitalize;
+  }
+  
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
+`
+
+export const S = {
+    Contact,
+    Form,
+    Field
+}
