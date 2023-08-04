323af91ca2dc5b5ef61ff8b8611e44b316804a2c
MrUnderSan <MrUnderSan@gmail.com>
2023-08-04 19:38:29 +0300

    add: add React Alice Carousel to Slider

--- src/components/slider/Slider_Styles.ts
+++ src/components/slider/Slider_Styles.ts
@@ -1,12 +1,19 @@
-import {theme} from "../../styles/Theme";
 import styled from "styled-components";
 
 const Slider = styled.div`
   max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 const Slide = styled.div`
   text-align: center;
+  cursor: grab;
+  &:active {
+    cursor: grabbing;
+  }
 `
 
 const Text = styled.p`
@@ -22,29 +29,9 @@ const Name = styled.span`
   margin: 22px 0 35px;
 `
 
-const Pagination = styled.div`
-  span {
-    display: inline-block;
-    width: 7px;
-    height: 7px;
-    border-radius: 20px;
-    background-color: rgba(255, 255, 255, 0.5);
-    
-    & + span {
-      margin-left: 5px;
-    }
-    
-    &.active {
-      background-color: ${theme.colors.accent};
-      width: 20px;
-    }
-  }
-`
-
 export const S = {
     Slider,
     Slide,
     Text,
-    Name,
-    Pagination
+    Name
 }
